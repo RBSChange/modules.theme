d5745553fb6c78b2de43faa25e774152c1d48927
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-09-20 10:01:01 +0200

    [UPDATE] update theme.init command

--- commands/dev/theme_Init.php
+++ commands/dev/theme_Init.php
@@ -29,7 +29,17 @@ class commands_theme_Init extends c_ChangescriptCommand
 	 */
 	protected function validateArgs($params, $options)
 	{
-		return count($params) == 1;
+		if (count($params) != 1)
+		{
+			return false;
+		}
+		$moduleName = $params[0];
+		if (!preg_match('/^[a-z][a-z0-9]{1,24}$/', $moduleName))
+		{
+			$this->errorMessage("Invalid theme name ([a-z][a-z0-9]{1,24}): " . $moduleName);
+			return false;
+		}
+		return true;
 	}
 
 	/**
@@ -56,21 +66,37 @@ class commands_theme_Init extends c_ChangescriptCommand
 		f_util_FileUtils::mkdir($themeDir."/modules");
 		f_util_FileUtils::mkdir($themeDir."/skin");
 		f_util_FileUtils::mkdir($themeDir."/style");
+		f_util_FileUtils::mkdir($themeDir."/setup");
 		f_util_FileUtils::mkdir($themeDir."/templates");
 		
 		$installDom = f_util_DOMUtils::fromString('<?xml version="1.0" encoding="UTF-8"?>
 <script>
   <binding fileName="modules/theme/persistentdocument/import/theme_binding.xml" />
   <rootfolder module="theme">
-    <theme id="'.$themeName.'" byCodename="'.$themeName.'" label="" description="">
+    <theme id="'.$themeName.'" byCodename="'.$themeName.'" label="'.$themeName.'" description="'.$themeName.'">
       <!-- 
       <pagetemplate byCodename="'.$themeName.'/fileNameWithoutExtension" doctype="XHTML-1.0-Strict|XHTML-1.0-Transitional" js="themes.'.$themeName.'.js..." useprojectcss="false|true" cssscreen="themes.'.$themeName.'..." />
       -->
     </theme>
     </rootfolder>
 </script>');
+		f_util_DOMUtils::save($installDom, $themeDir."/setup/init.xml");
+		
+		$package = c_Package::getNewInstance('themes', $themeName, PROJECT_HOME);
+		$package->setVersion(FRAMEWORK_VERSION);
+		$package->setDownloadURL('none');
+		
+		
+		$installDom = f_util_DOMUtils::fromString('<?xml version="1.0" encoding="UTF-8"?>
+<install></install>');
+		
+		$package->populateNode($installDom->documentElement);	
 		f_util_DOMUtils::save($installDom, $themeDir."/install.xml");
 		
-		return $this->quitOk("Theme $themeName initialized successfully. Please now edit files in themes/$themeName/.");
+		$this->getBootStrap()->updateProjectPackage($package);
+		
+		$this->executeCommand('theme.install', array($themeName));
+		
+		return $this->quitOk("Theme " . $package->__toString() . " initialized successfully. Please now edit files in ".PROJECT_HOME."/themes/$themeName/.");
 	}
 }
