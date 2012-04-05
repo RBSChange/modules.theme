9f94cdf2e95167def6df1d0ca2db00375918053d
Gaël PORT <gael.port@rbs.fr>
2012-04-05 12:36:33 +0200

    [NEW] Handle resizing in PreviewImageAction.

--- actions/PreviewImageAction.class.php
+++ actions/PreviewImageAction.class.php
@@ -1,31 +1,49 @@
 <?php
 class theme_PreviewImageAction extends f_action_BaseAction
 {
-	
 	/**
 	 * @param Context $context
 	 * @param Request $request
 	 */
-	public function _execute ($context, $request)
+	public function _execute($context, $request)
 	{
 		try
 		{
 			$document = $this->getDocumentInstanceFromRequest($request);
-			if ($document instanceof theme_persistentdocument_pagetemplate ||
-				$document instanceof theme_persistentdocument_theme) 
+			if ($document instanceof theme_persistentdocument_pagetemplate || $document instanceof theme_persistentdocument_theme)
 			{
 				$thumbnail = $document->getThumbnail();
 				if ($thumbnail)
 				{
 					$pathParts = explode('/', $thumbnail->getProjectpath());
-					$imagePath = FileResolver::getInstance()
-		        			->setPackageName($pathParts[0] . '_' . $pathParts[1])
-		        			->setDirectory($pathParts[2])
-		        			->getPath(implode(DIRECTORY_SEPARATOR, array_slice($pathParts, 3)));	
+					$imagePath = FileResolver::getInstance()->setPackageName($pathParts[0] . '_' . $pathParts[1])
+						->setDirectory($pathParts[2])->getPath(implode(DIRECTORY_SEPARATOR, array_slice($pathParts, 3)));
 					
-		        	MediaHelper::outputHeader($imagePath, null, false);
-					readfile($imagePath);
-					return View::NONE;	
+					// Handle thumbnail formating.
+					$formatSizeInfo = array();
+					if ($request->hasParameter('max-height'))
+					{
+						$formatSizeInfo['max-height'] = intval($request->getParameter('max-height'));
+					}
+					if ($request->hasParameter('max-width'))
+					{
+						$formatSizeInfo['max-width'] = intval($request->getParameter('max-width'));
+					}
+					
+					if (count($formatSizeInfo))
+					{
+						$resized = f_util_FileUtils::getTmpFile('thumbnail_') . '.' . f_util_ArrayUtils::lastElement(explode('.', $imagePath));
+						media_ResizerFormatter::getInstance()->resize($imagePath, $resized, $formatSizeInfo);
+						MediaHelper::outputHeader($resized, null, false);
+						readfile($resized);
+						f_util_FileUtils::unlink($resized);
+					}
+					else
+					{
+						MediaHelper::outputHeader($imagePath, null, false);
+						readfile($imagePath);
+					}
+					return View::NONE;
 				}
 			}
 		}
@@ -33,10 +51,13 @@ class theme_PreviewImageAction extends f_action_BaseAction
 		{
 			Framework::exception($e);
 		}
-        f_web_http_Header::setStatus(404);
-        return View::NONE;
-	}	
+		f_web_http_Header::setStatus(404);
+		return View::NONE;
+	}
 	
+	/**
+	 * @return boolean
+	 */
 	public function isSecure()
 	{
 		return true;
