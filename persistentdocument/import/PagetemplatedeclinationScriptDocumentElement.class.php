ba673e086b21e4eddc2f908d3347ced024e50d5b
Loïc Couturier <loic.couturier@rbs.fr>
2013-01-23 17:10:14 +0100

    [UPDATE] Add possiblity to edit template block in page template from an import

--- persistentdocument/import/PagetemplatedeclinationScriptDocumentElement.class.php
+++ persistentdocument/import/PagetemplatedeclinationScriptDocumentElement.class.php
@@ -21,42 +21,4 @@ class theme_PagetemplatedeclinationScriptDocumentElement extends theme_Pagetempl
 		return f_persistentdocument_PersistentDocumentModel::getInstanceFromDocumentModelName('modules_theme/pagetemplatedeclination');
 	}
 	
-	public function endProcess()
-	{
-		$children = $this->script->getChildren($this);
-		if (count($children))
-		{
-			$blockScripts = array();
-			foreach ($children as $child)
-			{
-				if ($child instanceof theme_ScriptTemplateblockElement)
-				{
-					$blockScripts[] = $child;
-				}
-			}
-			
-			if (count($blockScripts) > 0)
-			{
-				$declination = $this->getPersistentDocument();
-				$this->updateConfiguredBlocks($declination, $blockScripts);
-				$declination->save();
-			}
-		}
-	}
-	
-	/**
-	 * @param theme_persistentdocument_pagetemplatedeclination $declination
-	 * @param theme_ScriptTemplateblockElement[] $blockScripts
-	 */
-	protected function updateConfiguredBlocks($declination, $blockScripts)
-	{
-		$blocks = $declination->getConfiguredBlocks();
-		foreach ($blockScripts as $blockScript)
-		{
-			/* @var $blockScript theme_ScriptTemplateblockElement */
-			list($editname, $infos) = $blockScript->getBlockInfos();
-			$blocks[$editname] = $infos;
-		}
-		$declination->setConfiguredBlocks($blocks);
-	}
 }
