03a979db3d58379e01908e6f33ba7aac628a46d3
Gaël PORT <gael.port@rbs.fr>
2013-06-18 10:18:17 +0200

    [FIX] Theme GetImageAction.

--- actions/GetImageAction.class.php
+++ actions/GetImageAction.class.php
@@ -9,51 +9,62 @@ class theme_GetImageAction extends f_action_BaseAction
 	 * @param Request $request
 	 */
 	public function _execute($context, $request)
-    {
-        $path = $request->getParameter('path');   
-        try 
-        {  
-        	if (f_util_StringUtils::isNotEmpty($path))
-        	{
-	        	if (Framework::isInfoEnabled())
-	        	{
-	        		Framework::info(__METHOD__ . ':' . $path);
-	        	}
-	        	$pathParts = explode('/', $path);
-	        	if (count($pathParts) > 1)
-	        	{
-					$theme = $pathParts[0];					
-	        		$imagePath = FileResolver::getInstance()
-	        			->setPackageName('themes_' .$theme)
-	        			->setDirectory('image')
-	        			->getPath(implode(DIRECTORY_SEPARATOR, array_slice($pathParts, 1)));
-	        		if ($imagePath != null)
-	        		{
-	        			$link = f_util_FileUtils::buildWebeditPath('media', 'themes', $path);
-	        			f_util_FileUtils::mkdir(dirname($link));
-	        			f_util_FileUtils::symlink($imagePath, $link, f_util_FileUtils::OVERRIDE);	     			
-	        			MediaHelper::outputHeader($link, null, false);
-						readfile($link);
-						return View::NONE;
-	        		}
-	        	}
-        	}
-        }
-        catch (Exception $e)
-        {
-        	Framework::exception($e);
-        }
-        f_web_http_Header::setStatus(404);
-        return View::NONE;
-    }
+	{
+		$path = $request->getParameter('path');
+		try
+		{
+			if (f_util_StringUtils::isNotEmpty($path) && strtolower(substr($path, -4)) != '.php')
+			{
+				if (Framework::isInfoEnabled())
+				{
+					Framework::info(__METHOD__ . ':' . $path);
+				}
+				$pathParts = explode('/', $path);
+				if (count($pathParts) > 1)
+				{
+					$theme = $pathParts[0];
+					$imagePath = FileResolver::getInstance()
+						->setPackageName('themes_' . $theme)
+						->setDirectory('image')
+						->getPath(implode(DIRECTORY_SEPARATOR, array_slice($pathParts, 1)));
+					if ($imagePath != null)
+					{
+						// Validate that the image is really in themes/$theme/image.
+						$needle = DIRECTORY_SEPARATOR . 'themes' . DIRECTORY_SEPARATOR . $theme . DIRECTORY_SEPARATOR . 'image' . DIRECTORY_SEPARATOR;
+						if (false !== strpos(realpath($imagePath), $needle))
+						{
+							$link = f_util_FileUtils::buildWebeditPath('media', 'themes', $path);
+							f_util_FileUtils::mkdir(dirname($link));
+							f_util_FileUtils::symlink($imagePath, $link, f_util_FileUtils::OVERRIDE);
+							MediaHelper::outputHeader($link, null, false);
+							readfile($link);
+							return View::NONE;
+						}
+					}
+				}
+			}
+		}
+		catch (Exception $e)
+		{
+			Framework::exception($e);
+		}
+		f_web_http_Header::setStatus(404);
+		return View::NONE;
+	}
 
-    public function isSecure()
-    {
-    	return false;
-    }
+	/**
+	 * @return boolean
+	 */
+	public function isSecure()
+	{
+		return false;
+	}
 
-    public function getRequestMethods ()
-    {
-        return Request::GET;
-    }
+	/**
+	 * @return int|string
+	 */
+	public function getRequestMethods()
+	{
+		return Request::GET;
+	}
 }
